9ef64d2067697a3cd964cec9e85dd097b0f00fa6
youssef amin <youssefaminbeh@gmail.com>
2025-07-04 09:33:37 -0600

    god help me

---
+++ script.js
@@ -0,0 +1,56 @@
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+const CANVAS_WIDTH = window.innerWidth;
+const CANVAS_HEIGHT = window.innerHeight;
+canvas.width = CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT;
+
+var pictureX = 10;
+var pictureY = 10;
+var pictureWidth = 150;
+var pictureHeight = 200; 
+
+function createImages(path){
+    var img = new Image();
+    img.src = path;
+    return img;
+}
+
+var shreksSwamp = createImages('images/shreksSwamp.png');
+var images = [
+    createImages("images/shrek.png"),
+    createImages("images/shrek1.png"),
+    createImages("images/shrek2.png"),
+    createImages("images/shrek3.png"),
+    createImages("images/shrek4.png"),
+    createImages("images/shrek5.png"),
+    createImages("images/shrek6.png"),
+    createImages("images/shrek7.png"),
+]
+var mouse = {
+    x: undefined,
+    y: undefined
+}
+
+
+window.addEventListener('click', (e) => {
+    mouse.x = e.x;
+    mouse.y = e.y;
+    if(mouse.x <= pictureX + 150 || mouse.y <= pictureY + 200){
+        ranImage = Math.floor(Math.random() * images.length);
+        pictureX = Math.floor(Math.random() * (CANVAS_WIDTH - 150));
+        pictureY = Math.floor(Math.random() * (CANVAS_HEIGHT - 200));
+    }
+    
+});
+
+var ranImage = Math.floor(Math.random() * images.length);
+
+function animate(){   
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    ctx.drawImage(shreksSwamp, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    ctx.drawImage(images[ranImage], pictureX, pictureY, pictureWidth, pictureHeight);
+    requestAnimationFrame(animate);
+}
+
+animate();
